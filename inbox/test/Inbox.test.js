44a595fc3254121bcaa10d2714f13628101170d6
dd0sxx <contacttheotelonis@gmail.com>
2021-04-12 19:12:18 -0400

    added ganache and web3

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,3 +1,18 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
+const internal = require('stream');
 const Web3 = require('web3');
+const web3 = new Web3(ganache.provider()); 
+
+beforeEach(() => {
+    // get a list of all accounts
+    web3.eth.getAccounts().then(fetchedAccounts => {
+            console.log(fetchedAccounts);
+        });
+    // use one account to deploy the contract
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {});
+});
+
